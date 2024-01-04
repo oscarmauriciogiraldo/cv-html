809737530cbd1e62a83caf0f71c475b6df220bc6
oscarmauriciogiraldo <oscardan@utp.edu.co>
2024-01-04 13:10:19 -0500

    link active styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,8 +33,30 @@ function linkAction(){
 navLink.forEach(element => element.addEventListener('click', linkAction))
 
 /**
- *
  * @author: OScar Giraldo
  * @name: Scroll active
- * @description: 
+ * @description: This function manipulates the scroll; 
+ * upon clicking on the icon of any section, whether 
+ * it's Profile, Certifications, etc., the scroll 
+ * either scrolls down or up to that section
 */
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.navMenu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.navMenu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+
+    })
+
+}
+window.addEventListener('scroll', scrollActive)
